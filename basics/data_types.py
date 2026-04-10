d9d5cc8b78a21a0a280c76b15ca7ad48a9d1c6d5
Tauqeer Darve <91264028+tauqeer-darve@users.noreply.github.com>
2026-04-10 22:04:13 +0530

    Data types basics

---
+++ basics/data_types.py
@@ -0,0 +1,8 @@
+text = 'apple'
+number = 10
+decimal = -10.123
+has_money = True
+coordinates = (2.5, 1.5)
+names = ['Agnetha', 'Björn', 'Benny', 'Anni-Frid']
+unique = {1, 2, 3, 4, 4, 5}
+users = {'Bob': 1, 'James': 2}
