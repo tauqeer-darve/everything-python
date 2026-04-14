61d6963cbf24f58ec2ba31dc1e5850b59c427d95
Tauqeer Darve <91264028+tauqeer-darve@users.noreply.github.com>
2026-04-14 13:32:27 +0000

    loops basics

---
+++ basics/loops.py
@@ -0,0 +1,19 @@
+#For Loops
+for i in range(3):
+    print("Hello")
+
+names: list[str] = ['Agnetha', 'Björn', 'Benny', 'Anni-Frid']  # list of strings
+
+for name in names:
+    print(f'Hello, {name}')
+
+#While Loops
+i: int = 0
+while i <=10:
+    print(i)
+    i+=1
+
+x: int = 5
+while x:
+    print(x)
+    x-=1
