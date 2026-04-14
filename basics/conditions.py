c295b5af31a94e87343fde396779a8f08348b8dd
Tauqeer Darve <91264028+tauqeer-darve@users.noreply.github.com>
2026-04-14 13:40:07 +0000

    Conditional Statements

---
+++ basics/conditions.py
@@ -0,0 +1,8 @@
+user_input: str = 'hello'
+
+if user_input == 'hello':
+    print('Bot: Hello!')
+elif user_input == 'How are you?':
+    print('Bot: Good, what about you')
+else:
+    print('Bot: Sorry, I did not understand that.')
